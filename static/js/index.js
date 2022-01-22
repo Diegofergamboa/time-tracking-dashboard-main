49fdd1ffcc642962ebd0183b96f21db3c163d2be
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-01-22 14:25:55 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,33 @@
 // import the data from json file
 // Organize the divs
 
-fetch
+// fetch
+
+document.addEventListener("DOMContentLoaded" , () => {
+    loadActivity();
+});
+
+const task = document.getElementsByClassName('div-task');
+
+function getActivity () {
+    const url = "/data.json";
+    return fetch(url)
+    .then(response => {
+        return response.json();
+    }).then((result) => {
+        return result;
+    }).catch((err) => {
+        console.log('ERROR CON FETCH FUNCTION');
+    });
+}
+async function loadActivity() {
+    const activities = await getActivity();
+    let html = '';
+    activities.forEach(activity => {
+        html += `
+            <div></div>
+            <div></div>
+        `
+    });(activity => {
+
+    })
