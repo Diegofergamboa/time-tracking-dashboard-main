3ab9e2cf3ee864317d92db2605b26bfb131e883a
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-01-22 19:04:16 -0500

    Printed all the data of the json file

--- static/js/index.js
+++ static/js/index.js
@@ -3,41 +3,42 @@
 
 // fetch
 document.addEventListener("ONContentLoaded", 
-    loadDailyActivity() 
+    // getActivity() 
+    loadDailyActivity()
 );
 
 // loadMonthlyActivity();
 // loadWeeklyActivity();
-// loadDailyActivity();
-
-getActivity ()
-const task = document.getElementsByClassName('div-task');
 
 function getActivity () {
-    const url = "/data.json";
+    const url = "../data.json";
+    
     return fetch(url)
     .then(response => {
         return response.json();
     }).then((result) => {
-        return result;
+        return result
     }).catch((err) => {
-        console.log('ERROR CON FETCH FUNCTION');
+        console.log('Error on Fetch Function');
     });
 }
+
+const task = document.getElementsByClassName('div-task');
 async function loadDailyActivity() {
-    const activities = await getActivity();
-    let html = '';
-    activities.forEach(activity => {
-        html += `
-            <div>
-                <p>${activity.title}</p>
-                <p>${activity.current}hrs</p>
-            </div>
-            <div>
-                <img src="/images/icon-ellipsis.svg" alt="imagen de puntos suspensivos">
-                <p>Last Daily - ${activity.previous}</p>
-            </div>
-        `
-    });
-    document.getElementsByClassName("task")[0].innerHTML = html;
-)})
+    const objectActivities = await getActivity();
+    const listTask = objectActivities.map(activity => {
+        // console.log(activity.timeframes.daily)
+        return  ` <div>
+                    <p>${activity.title}</p>
+                    <p>${activity.timeframes.daily.current}hrs</p>
+                </div>
+                <div>
+                    <img src="/images/icon-ellipsis.svg" alt="imagen de puntos suspensivos">
+                    <p>Last Daily - ${activity.timeframes.daily.previous}</p>
+                </div>
+                `
+        })
+        task[0].innerHTML = listTask.join('');
+    }
+//     });
+//     document.getElementsByClassName("task")[0].innerHTML = html;
